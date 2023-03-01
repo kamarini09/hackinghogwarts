4608c77773a7618d3a4cfa00eb2f3feb1c744be9
kamarini <kama0549@stud.kea.dk>
2023-03-01 14:23:08 +0100

    trying perfect

--- cleandata.js
+++ cleandata.js
@@ -1,5 +1,6 @@
 "use strict";
 import { getBloodStatus } from "./bloodstatus.js";
+//import { tryToMakeAPrefect } from "./prefect.js";
 
 
 const endpoint = "https://petlatkea.dk/2021/hogwarts/students.json";
@@ -66,11 +67,12 @@ function prepareObjects(jsonData) {
 function displayStudent(student) {
   // create clone
   const clone = document.querySelector("template#student").content.cloneNode(true);
+
   if(student.squad){
-    clone.querySelector("[data-field=squad]").textContent = "⭐";
-} else {
+    clone.querySelector("[data-field=squad]").textContent = "⭐";  
+  } else {
     clone.querySelector("[data-field=squad]").textContent = "☆";
-}
+  }
   // set clone data
   clone.querySelector("#image").src = student.image;
   clone.querySelector("[data-field=firstName").textContent = student.firstname;
@@ -80,24 +82,43 @@ function displayStudent(student) {
   clone.querySelector("[data-field=gender").textContent = student.gender;
   clone.querySelector("[data-field=house]").textContent = student.house;
   clone.querySelector("[data-field=bloodStatus]").textContent = student.bloodstatus;
-  clone.querySelector("[data-field=squad]").addEventListener(
-    `click`, addToSquad);
+
+  //add someone to the squad
+  clone.querySelector("[data-field=squad]").addEventListener(`click`, addToSquad);
     function addToSquad(){
       if(student.bloodstatus ==="Pure Blood" || student.house ==="Slytherin"){
         student.squad = !student.squad;
        }else{
          alert("you cannott")
        }
-  
+      }
+
+  //put a student in prefect
+  clone.querySelector("[data-field=prefects]").dataset.prefect = student.prefect;
+  clone.querySelector("[data-field=prefects]").addEventListener(`click`, makePrefect);
+
+    function makePrefect(){
+       console.log("im in makePrefect function")
+        // untoggle an animal is always possible, but not toggle it (2 winners for each category)
+        if(student.prefect === true){
+            student.prefect = false;
+        } else {
+            tryToMakeAPrefect(student);
+            console.log("im in the else")
+        }
+        //buildList();
+        //displayList(); //??????????????????
+    displayList();
+    }
       
-      displayList();
-    };
+     
+    
   
 
   // append clone to list
   document.querySelector("#list tbody").appendChild(clone);
-}
 
+  }
 
 //------------------------CONTROLER-----------------------------------
 function makeFirstCapital(x){
@@ -164,4 +185,94 @@ function putImage(lastname, firstname){
   return `images/${lastname.toLowerCase()}_${firstname[0].toLowerCase()}.png`
   }
 
+}
+
+function tryToMakeAPrefect(selectedStudent){
+  const prefects = allStudents.filter(student => student.prefect)
+  const numberOfPrefects = prefects.length;
+  const other = prefects.filter(student => student.house === selectedStudent.house ).shift();
+  // if there is another of the same type
+  if (other !== undefined){
+      console.log("there can only be one prefect of each house!");
+      removeOther(other);
+  } else if (numberOfPrefects >= 3 ){ //testing
+      console.log("there can only be two prefects");
+      removeAorB(prefects[0], prefects[1]);
+  } else {
+      makePrefect(selectedStudent);
+  }
+
+  makePrefect(selectedStudent);
+
+
+function removeOther(other){
+  // show name on button
+  document.querySelector("#onlyonekind p button span").textContent =`${other.firstname}, the ${other.house}`;
+
+  // ask the user to ignore or remove the other
+  document.querySelector("#onlyonekind").classList.remove("hide");
+  document.querySelector("#onlyonekind .closebutton").addEventListener("click", closeDialog);
+  document.querySelector("#onlyonekind p button").addEventListener("click", clickRemoveOther);
+  // if ignore, do nothing (remove the event listeneras good practice)
+  function closeDialog(){
+  document.querySelector("#onlyonekind").classList.add("hide");
+  document.querySelector("#onlyonekind .closebutton").removeEventListener("click", closeDialog);
+  document.querySelector("#onlyonekind p button").removeEventListener("click", clickRemoveOther);
+  }
+
+function clickRemoveOther(){
+  removePrefect(other);
+  makePrefect(selectedStudent);
+  // buildList();
+  displayList();
+  closeDialog();
+  }
+  }
+  
+function removeAorB(studentA, studentB){
+  // show names on buttons
+  document.querySelector("#onlytwowinners [data-action=remove1] span").textContent =`${studentA.name}, the ${studentA.house}`;
+  document.querySelector("#onlytwowinners [data-action=remove2] span").textContent = `${studentB.name}, the ${studentB.house}`;
+
+  // ask the user to ignore or remove 'A or B
+  document.querySelector("#onlytwowinners").classList.remove("hide");
+  document.querySelector("#onlytwowinners .closebutton").addEventListener("click", closeDialog);
+  document.querySelector("#onlytwowinners [data-action=remove1]").addEventListener("click", clickRemoveA);
+  document.querySelector("#onlytwowinners [data-action=remove2]").addEventListener("click", clickRemoveB);
+
+  // if ignore, do nothing
+  function closeDialog(){
+  document.querySelector("#onlytwowinners").classList.add("hide");
+  document.querySelector("#onlytwowinners .closebutton").removeEventListener("click", closeDialog);
+  document.querySelector("#onlytwowinners [data-action=remove1]").removeEventListener("click", clickRemoveA);
+  document.querySelector("#onlytwowinners [data-action=remove2]").removeEventListener("click", clickRemoveB);
+  }
+
+function clickRemoveA(){
+      removePrefect(studentA);
+      makePrefect(selectedStudent);
+      // buildList();
+      displayList();
+      closeDialog();
+  }
+
+function clickRemoveB(){
+  // else - if removeB
+  removePrefect(studentB);
+  makePrefect(selectedStudent);
+  // buildList();
+  displayList();
+  closeDialog();
+  }
+   
+}
+
+function removePrefect(prefectStudent){
+  console.log("remove prefect");
+  prefectStudent.prefect = false;
+}
+
+function makePrefect(student){
+  student.prefect = true;
+}
 }
