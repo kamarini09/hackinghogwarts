a006714bca55db3ef13a9bb32337c7cefe80828a
kamarini <kama0549@stud.kea.dk>
2023-03-12 13:45:26 +0100

    image problem

--- cleandata.js
+++ cleandata.js
@@ -383,6 +383,7 @@ function putImage(lastname, firstname){
   
   }
   else if(lastname.includes('-')){
+   
     return `images/${lastname.substring(lastname.indexOf("-") +1 ).toLowerCase()}_${firstname.charAt(0).toLowerCase()}.png`
   }
   else {
